1f59d32800a7f3654caa9780c79b518da2b0703c
pixsperdavid <david@pixsper.com>
2022-07-27 23:02:24 +0100

    Added additional unit test output for C backend

--- scripts/update_ui_tests.sh
+++ scripts/update_ui_tests.sh
@@ -4,7 +4,8 @@
 
 PROJECT_ROOT="$( cd "$(dirname "$0")/.." ; pwd -P )" # this file
 
-cp "$PROJECT_ROOT"/backends/c/tests/output/my_header.h "$PROJECT_ROOT"/backends/c/tests/output/my_header.h.expected
+cp "$PROJECT_ROOT"/backends/c/tests/output_nodocs/my_header.h "$PROJECT_ROOT"/backends/c/tests/output_nodocs/my_header.h.expected
+cp "$PROJECT_ROOT"/backends/c/tests/output_docs_inline/my_header.h "$PROJECT_ROOT"/backends/c/tests/output_docs_inline/my_header.h.expected
 cp "$PROJECT_ROOT"/backends/cpython/tests/output/reference_project.py "$PROJECT_ROOT"/backends/cpython/tests/output/reference_project.py.expected
 cp "$PROJECT_ROOT"/backends/csharp/tests/output_safe/Interop.cs "$PROJECT_ROOT"/backends/csharp/tests/output_safe/Interop.cs.expected
 cp "$PROJECT_ROOT"/backends/csharp/tests/output_safe/Interop.common.cs "$PROJECT_ROOT"/backends/csharp/tests/output_safe/Interop.common.cs.expected
