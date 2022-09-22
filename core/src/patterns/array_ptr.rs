2bed634f63fb9d7f4ec653ca1ba72db624a3c622
pixsperdavid <david@pixsper.com>
2022-09-22 11:19:18 +0100

    Array Pointer fixes
    - Made representation transparent
    - Marked `as_slice` function unsafe

--- core/src/patterns/array_ptr.rs
+++ core/src/patterns/array_ptr.rs
@@ -3,7 +3,7 @@ use crate::lang::rust::CTypeInfo;
 use crate::patterns::TypePattern;
 use std::marker::PhantomData;
 
-#[repr(C)]
+#[repr(transparent)]
 #[cfg_attr(feature = "serde", derive(Debug, Copy, Clone, PartialEq, Deserialize, Serialize))]
 #[cfg_attr(not(feature = "serde"), derive(Debug, Copy, Clone, PartialEq))]
 pub struct ArrayPointer<'a, T> {
@@ -31,16 +31,15 @@ where
 }
 
 impl<'a, T> ArrayPointer<'a, T> {
-    pub fn as_slice<'b>(&'b self, len: u64) -> &'b [T]
+    pub unsafe fn as_slice<'b>(&'b self, len: u64) -> &'b [T]
     where
         'a: 'b,
     {
         if self.data.is_null() {
             &[]
         } else {
-            // If non-null this should always point to valid data and the lifetime should be
-            // guaranteed via the struct <'a>.
-            unsafe { std::slice::from_raw_parts(self.data, len as usize) }
+            // Dependent on user passing in the correct length, hence function marked as unsafe
+            std::slice::from_raw_parts(self.data, len as usize)
         }
     }
 }
