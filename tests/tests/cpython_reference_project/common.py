cc3996f20c9b848ce526eaaf3aa1e224d003ae18
Marcel Müller <neikos@neikos.email>
2024-11-25 19:09:07 +0100

    Try to detect platform and use that to pre/suffix the shared library object
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- tests/tests/cpython_reference_project/common.py
+++ tests/tests/cpython_reference_project/common.py
@@ -1,1 +1,16 @@
-DLL = "../../../target/debug/interoptopus_reference_project.dll"
+import platform
+
+if platform.uname()[0] == "Linux":
+    prefix = "lib"
+else:
+    prefix = ""
+
+
+if platform.uname()[0] == "Windows":
+    suffix = ".dll"
+elif platform.uname()[0] == "Linux":
+    suffix = ".so"
+else:
+    suffix = ""
+
+DLL = "../../../target/debug/" + prefix + "interoptopus_reference_project" + suffix
