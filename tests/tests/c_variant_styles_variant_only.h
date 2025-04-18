57bed9b5aaaae82864b25d140558ac2fe31dcd7d
Angad Tendulkar <angad.tendulkar@gmail.com>
2025-04-17 22:23:44 -0400

    tests

---
+++ tests/tests/c_variant_styles_variant_only.h
@@ -0,0 +1,30 @@
+
+
+#ifndef interoptopus_generated
+#define interoptopus_generated
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/types.h>
+
+
+
+
+typedef enum COLOR
+    {
+    RED = 0,
+    GREEN = 1,
+    BLUE = 2,
+    } COLOR;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* interoptopus_generated */
---
+++ tests/tests/c_variant_styles_with_name.h
@@ -0,0 +1,30 @@
+
+
+#ifndef interoptopus_generated
+#define interoptopus_generated
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/types.h>
+
+
+
+
+typedef enum COLOR
+    {
+    COLOR_RED = 0,
+    COLOR_GREEN = 1,
+    COLOR_BLUE = 2,
+    } COLOR;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* interoptopus_generated */
