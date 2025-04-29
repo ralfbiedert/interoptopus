9fad7376cf30b017b14906f6ba65d1343ee896ff
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-04-29 19:16:51 +0300

    wip: move protos to single dir

--- benches/protobuf/build.rs
+++ benches/protobuf/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let proto_files = ["Input.proto", "Outputs.proto"];
-    let include_paths = ["./models1", "./models2"];
+    let include_paths = ["./models"];
 
     for file in &proto_files {
         println!("cargo:rerun-if-changed={file}");
