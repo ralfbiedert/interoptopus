0760c9b41670bfe98eee7a8f42386e3f49a85b9f
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-04-29 19:17:05 +0300

    wip: list benchy variants

---
+++ benches/protobuf/src/lib.rs
@@ -0,0 +1,5 @@
+// This is rust_bench.dll doing two variants of the API
+// - one is Protobuf ser/de based
+// - one is Wire<T> based
+// The Wire<T> version does NOT need any protobuf files, and is defined
+// solely using Rust types.
