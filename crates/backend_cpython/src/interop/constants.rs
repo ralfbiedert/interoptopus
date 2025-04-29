bc4e5ce8d6d48f9a66a26de47c6142f840325e8c
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-04-29 12:13:44 +0300

    wip: render constants from template

--- crates/backend_cpython/src/interop/constants.rs
+++ crates/backend_cpython/src/interop/constants.rs
@@ -1,12 +1,21 @@
 use crate::Interop;
 use crate::converter::constant_value_to_value;
+use interoptopus::Error;
 use interoptopus::backend::IndentWriter;
-use interoptopus::{Error, indented};
+use std::collections::HashMap;
+use tera::Context;
 
 pub fn write_constants(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
-    for c in i.inventory.constants() {
-        indented!(w, r"{} = {}", c.name(), constant_value_to_value(c.value()))?;
-    }
+    let constants = i
+        .inventory
+        .constants()
+        .into_iter()
+        .map(|c| (c.name(), constant_value_to_value(c.value())))
+        .collect::<HashMap<_, _>>();
 
+    //render!(w, "constants.py", ("constants", &constants));
+    let mut context = Context::new();
+    context.insert("constants", &constants);
+    crate::TEMPLATES.render_to("constants.py", &context, w.writer()).unwrap();
     Ok(())
 }
