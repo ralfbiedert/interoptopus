8c97664be739ead2715318fb05af98c3dbff5b93
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-05-09 12:29:40 +0300

    move as a mod under tests/ following csharp_benchmarks

--- benches/protobuf-vs-wire/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// This is rust_bench.dll doing two variants of the API
-// - one is Protobuf ser/de based
-// - one is Wire<T> based
-// The Wire<T> version does NOT need any protobuf files, and is defined
-// solely using Rust types.
-// mod models;
-mod proto;
-mod wire;
-
