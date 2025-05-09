1cd26f815b8a86072e99cb273054b0d4147cb12b
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-05-09 12:29:07 +0300

    [SPLIT to WIRE] add two impls

--- benches/protobuf-vs-wire/ForCSharp/ForCSharp/Program.cs
+++ benches/protobuf-vs-wire/ForCSharp/ForCSharp/Program.cs
@@ -1,26 +1,20 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Google.Protobuf;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Threading;
 
 namespace One.Two.ForCSharp;
 
 // Pure protobuf-based interop between Rust and C#.
-public class Interop
+public class InteropProtobuf
 {
     [DllImport("rust_bench.dll")]
-    private static extern int DummyFunction(int numOne, int numTwo);
-
-    [DllImport("rust_bench.dll")]
-    private static unsafe extern void RustClient(byte[] structPointer, uint structLength, void** result, uint* resultLength);
+    private static unsafe extern void ProtoRustClient(byte[] structPointer, uint structLength, void** result, uint* resultLength);
 
     [DllImport("rust_bench.dll")]
     private static unsafe extern void FreeRustResultMemory(byte* rustPtr, uint len);
-
-    /// Test basic interop.
-    public static int ExecuteDummyFunction(int numOne, int numTwo)
-    {
-        return DummyFunction(numOne, numTwo);
-    }
 
     /// Main benched function.
     public static Outputs ExecuteRustClient(Input input)
@@ -35,7 +29,7 @@ public class Interop
 
                 byte* resultPtr = null;
                 uint resultBufLength = 0;
-                RustClient(byteArray, (uint)byteArray.Length, (void**)&resultPtr, &resultBufLength);
+                ProtoRustClient(byteArray, (uint)byteArray.Length, (void**)&resultPtr, &resultBufLength);
 
                 // Start the deserialization timer
                 Stopwatch deser = Stopwatch.StartNew();
@@ -83,5 +77,89 @@ public class Interop
         }
 
         return result;
+    }
+}
+
+// Interoptopus Wire<T>-based interop between Rust and C#.
+public class InteropWire
+{
+    [DllImport("rust_bench.dll")]
+    private static unsafe extern void WireRustClient(byte[] structPointer, uint structLength, void** result, uint* resultLength);
+
+    [DllImport("rust_bench.dll")]
+    private static unsafe extern void FreeRustResultMemory(byte* rustPtr, uint len);
+
+    /// Main benched function.
+    public static Outputs ExecuteRustClient(Input input)
+    {
+        try
+        {
+            unsafe
+            {
+                byte[] byteArray = input.ToWire().ToByteArray();
+
+                byte* resultPtr = null;
+                uint resultBufLength = 0;
+                WireRustClient(byteArray, (uint)byteArray.Length, (void**)&resultPtr, &resultBufLength);
+
+                byte[] result = CopyAndDeallocate(resultPtr, resultBufLength);
+
+                Outputs output = Outputs.Parser.ParseFrom(result);
+
+                return output;
+            }
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException($"Unexpected error in ExecuteRustClient: {e.Message}", e);
+        }
+    }
+
+    /// <remarks>
+    /// Since the memory is deallocated after copying, the contentPtr should not be accessed
+    /// again after calling this method. Ensuring that the pointer
+    /// is valid and that the given length correctly represents the allocated memory size when using this method.
+    /// </remarks>
+    /// <returns>The content at the pointer location. Null if the contentPtr is null.</returns>
+    public static unsafe byte[] CopyAndDeallocate(byte* contentPtr, uint contentLength)
+    {
+        byte[] result = Array.Empty<byte>();
+
+        if (contentPtr != null)
+        {
+            try
+            {
+                result = new byte[contentLength];
+                Marshal.Copy((IntPtr)contentPtr, result, 0, (int)contentLength);
+            }
+            finally
+            {
+                //Free rust memory
+                FreeRustResultMemory(contentPtr, contentLength);
+            }
+        }
+
+        return result;
+    }
+}
+
+[RPlotExporter]
+public class Benchy {
+    [Benchmark]
+    public void ProtobufInterop() {
+        var outputs = InteropProtobuf.ExecuteRustClient(input);
+    }
+
+    [Benchmark]
+    public void WireInterop() {
+        var outputs = InteropWire.ExecuteRustClient(input);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchy>();
     }
 }
