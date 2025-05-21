457d508270cc38ee3199916cafbbeef7d91cbf57
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-05-21 20:45:10 +0300

    [SPLIT to WIRE] [temp] wip

--- tests/tests/csharp_benchmarks_protobuf/Program.cs
+++ tests/tests/csharp_benchmarks_protobuf/Program.cs
@@ -10,18 +10,19 @@ public class Benchy
     [Benchmark]
     public void ProtobufInterop()
     {
+        var input = new Input();
         // TODO: generate input based on benchmark parameters
         // (at least two extremes: very small payload, very large payload and maybe something in-between)
         var outputs = InteropProtobuf.ExecuteRustClient(input);
     }
 
-    [Benchmark]
-    public void WireInterop()
-    {
-        // TODO: generate input based on benchmark parameters
-        // (at least two extremes: very small payload, very large payload and maybe something in-between)
-        var outputs = InteropWire.ExecuteRustClient(input);
-    }
+    // [Benchmark]
+    // public void WireInterop()
+    // {
+    //     // TODO: generate input based on benchmark parameters
+    //     // (at least two extremes: very small payload, very large payload and maybe something in-between)
+    //     var outputs = InteropWire.ExecuteRustClient(input);
+    // }
 }
 
 class Program
