f99626178aecf7443d46bbe30bea00f62ff2eb3c
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-05-21 20:42:02 +0300

    make proto files usable from rust and c# both

--- tests/tests/csharp_benchmarks_protobuf/build.rs
+++ tests/tests/csharp_benchmarks_protobuf/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let proto_files = ["Input.proto", "Outputs.proto"];
-    let include_paths = ["./models"];
+    let include_paths = ["/.", "./models"];
 
     for file in &proto_files {
         println!("cargo:rerun-if-changed={file}");
