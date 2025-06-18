4a98ee28cbd2fc943556548908ad597d5384c375
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-06-18 15:03:32 +0300

    magic fun

--- crates/reference_project/src/types/wired.rs
+++ crates/reference_project/src/types/wired.rs
@@ -7,11 +7,13 @@ struct MyWiredType {
     attribute: (u32, String, HashMap<String, u8>),
 }
 
+// input is a serialized representation, parse it to access MyWiredType.
+// serialize resulting MyWiredType into a buffer and return it as WireOfMyWiredType on C# side
 #[ffi_function]
 fn perform_miracles(input: Wire<MyWiredType>) -> Wire<MyWiredType> {}
 
 #[ffi_function]
-fn perform_half_miracles(input: Wire<MyWiredType>) -> ffi::String {}
+fn perform_half_miracles(input: Wire<MyWiredType>, other: ffi::String) -> ffi::String {}
 
 #[ffi_function]
 fn perform_half_miracles_in_other_direction(input: ffi::String) -> Wire<MyWiredType> {}
