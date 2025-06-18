46db60e973e5a0309c32af09e3b85b6212797ce8
Berkus Karchebnyi <berkus.karchebnyi@microsoft.com>
2025-06-18 15:25:47 +0300

    fix typo

--- crates/backend_csharp/src/interop/builtins/async_helper.rs
+++ crates/backend_csharp/src/interop/builtins/async_helper.rs
@@ -4,7 +4,7 @@ use interoptopus::backend::IndentWriter;
 
 pub fn write_async_helper(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
     if i.write_types.write_interoptopus_globals() {
-        render!(w, "builtins/async_helpers.cs")?;
+        render!(w, "builtins/async_helper.cs")?;
     }
     Ok(())
 }
