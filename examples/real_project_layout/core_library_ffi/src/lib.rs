e3b1ba6a2c935250f26e1fc8ebe077a0b13d09ad
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-07-15 15:07:36 +0300

    clean up

--- examples/real_project_layout/core_library_ffi/src/lib.rs
+++ examples/real_project_layout/core_library_ffi/src/lib.rs
@@ -20,11 +20,9 @@ pub struct Return {
 // As in `engine`, we create matching functions that are better suited for an FFI boundary.
 #[ffi_function(debug)]
 pub fn start_server(mut server_name: Wire<Something>) -> Wire<Return> {
-    // ^^ register should traverse Wire types recursively and add them to inventory
-    // TypeInfo::wire_type_info() perhaps
-    // NB: will c_types and wire_types ever mingle? --
+    // ^^ @todo: register should traverse Wire types recursively and add them to inventory
     if server_name.name.is_empty() {
-        return Return { field: 0 }; // @todo: This should generate Wire<>
+        return Return { field: 0 }; // This will generate a Wire<Return>
     };
 
     core_library::start_server(server_name.name.to_string());
