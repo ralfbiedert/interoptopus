8b769d390f61473a00cf4d47e7fd6a42332ce2ae
nelson <nelson@truran.dev>
2025-07-20 23:16:16 -0700

    fix const name ref

--- crates/backend_csharp/src/interop/builtins/utf8string.rs
+++ crates/backend_csharp/src/interop/builtins/utf8string.rs
@@ -4,7 +4,8 @@ use interoptopus::{Error, render};
 
 pub fn write_utf8_string(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
     if i.write_types.write_interoptopus_globals() {
-        render!(w, "builtins/utf8string.cs")?;
+        let class_name = i.class_constants.as_ref().map_or(&i.class, |name| name);
+        render!(w, "builtins/utf8string.cs", ("class_name", class_name))?;
     }
     Ok(())
 }
