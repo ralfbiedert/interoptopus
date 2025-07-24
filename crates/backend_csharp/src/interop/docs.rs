ec8ab78b308d990862da8db94800f928aebca9f3
Berkus Decker <berkus+github@metta.systems>
2025-07-24 14:38:12 +0300

    ✨ Render Interop.cs file header from template

--- crates/backend_csharp/src/interop/docs.rs
+++ crates/backend_csharp/src/interop/docs.rs
@@ -2,7 +2,7 @@ use crate::Interop;
 use interoptopus::backend::IndentWriter;
 use interoptopus::lang::Docs;
 use interoptopus::pattern::api_guard::ApiHash;
-use interoptopus::{Error, indented};
+use interoptopus::{Error, indented, render};
 
 const INTEROPTOPUS_CRATE: &str = env!("CARGO_PKG_NAME");
 const INTEROPTOPUS_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -11,15 +11,26 @@ pub fn write_file_header_comments(i: &Interop, w: &mut IndentWriter) -> Result<(
     let hash = ApiHash::from(&i.inventory);
     let hash_hex = hash.hash_hex();
 
-    let header = &i.file_header_comment;
-    let header = header.replace("{INTEROP_HASH}", hash_hex);
-    let header = header.replace("{INTEROP_DLL_NAME}", &i.dll_name);
-    let header = header.replace("{INTEROP_NAMESPACE}", &i.namespace_id);
-    let header = header.replace("{INTEROPTOPUS_CRATE}", INTEROPTOPUS_CRATE);
-    let header = header.replace("{INTEROPTOPUS_VERSION}", INTEROPTOPUS_VERSION);
-
-    indented!(w, r"{}", header)?;
-    Ok(())
+    if let Some(header) = &i.file_header_comment {
+        let mut context = tera::Context::new();
+        context.insert("INTEROP_HASH", hash_hex);
+        context.insert("INTEROP_DLL_NAME", &i.dll_name);
+        context.insert("INTEROP_NAMESPACE", &i.namespace_id);
+        context.insert("INTEROPTOPUS_CRATE", INTEROPTOPUS_CRATE);
+        context.insert("INTEROPTOPUS_VERSION", INTEROPTOPUS_VERSION);
+        let rendered = tera::Tera::one_off(header, &context, true).map_err(Error::Templating)?;
+        indented!(w, "{}", rendered)
+    } else {
+        render!(
+            w,
+            "file_header.cs",
+            ("INTEROP_HASH", hash_hex),
+            ("INTEROP_DLL_NAME", &i.dll_name),
+            ("INTEROP_NAMESPACE", &i.namespace_id),
+            ("INTEROPTOPUS_CRATE", INTEROPTOPUS_CRATE),
+            ("INTEROPTOPUS_VERSION", INTEROPTOPUS_VERSION)
+        )
+    }
 }
 
 pub fn write_documentation(w: &mut IndentWriter, documentation: &Docs) -> Result<(), Error> {
