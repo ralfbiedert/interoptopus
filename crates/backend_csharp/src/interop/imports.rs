7fe94e82e4760695aca9d684bb3456713291ce1f
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-08-05 12:34:18 +0300

    [templates 📟]  Add wired types imports to generated template

--- crates/backend_csharp/src/interop/imports.rs
+++ crates/backend_csharp/src/interop/imports.rs
@@ -5,9 +5,11 @@ use interoptopus::{Error, render};
 pub fn write_imports(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
     i.debug(w, "write_imports")?;
 
+    let is_wired = i.has_emittable_wired_types();
+
     let mut namespace_imports = i.namespace_mappings.iter().map(|x| x.1.as_str().to_string()).collect::<Vec<_>>();
     namespace_imports.sort();
     namespace_imports.dedup();
 
-    render!(w, "imports.cs", ("namespace_imports", &namespace_imports))
+    render!(w, "imports.cs", ("namespace_imports", &namespace_imports), ("is_wired", &is_wired))
 }
