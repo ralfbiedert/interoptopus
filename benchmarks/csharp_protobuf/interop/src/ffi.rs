198f2016b39fd0d24dac7685ee19ff3c70f93540
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-08-09 00:26:43 +0300

    Namespaces in benchy ❌ generate Wire<T> in the right namespace | 🏎 benchy

--- benchmarks/csharp_protobuf/interop/src/ffi.rs
+++ benchmarks/csharp_protobuf/interop/src/ffi.rs
@@ -1,8 +1,8 @@
 use interoptopus::{ffi, ffi_function, ffi_type};
 
 /// Main benchmark Rust entry point for FFI-based interop.
-#[ffi_function]
-pub fn FfiRustClient(_input: Input) -> Outputs {
+#[ffi_function(namespace = "ffi")]
+pub fn FfiRustClient(_input: FInput) -> FOutputs {
     /*    println!("PRINTLN DEBUG IS THA BEST");
     println!("NUMBERS VALIDITY CHECK:");
     println!("response_size = {}", _input.configuration.response_size);
@@ -13,94 +13,94 @@ pub fn FfiRustClient(_input: Input) -> Outputs {
     println!("HOST RECEIVED: {:?}", _input.configuration.host);*/
 
     // TODO: use input.response_size to generate outputs
-    let results = vec![Result { item_value: 42, item_id: ffi::String::from("item1".to_string()) }];
-    let items = vec![Item { key: ItemKey::TOTAL, value: 100 }];
-    Outputs {
-        response: Response { results: ffi::Vec::from_vec(results) },
-        data: Data { items: Items { items: ffi::Vec::from_vec(items) }, errors: Error { error_messages: ffi::Vec::from_vec(Vec::<ffi::String>::new()) } },
+    let results = vec![FResult { item_value: 42, item_id: ffi::String::from("item1".to_string()) }];
+    let items = vec![FItem { key: FItemKey::TOTAL, value: 100 }];
+    FOutputs {
+        response: FResponse { results: ffi::Vec::from_vec(results) },
+        data: FData { items: FItems { items: ffi::Vec::from_vec(items) }, errors: FError { error_messages: ffi::Vec::from_vec(Vec::<ffi::String>::new()) } },
     }
 }
 
-#[ffi_type]
-pub struct Input<'l> {
-    pub context: Context<'l>,
-    pub value: Table<'l>,
-    pub configuration: Configuration,
+#[ffi_type(namespace = "ffi")]
+pub struct FInput<'l> {
+    pub context: FContext<'l>,
+    pub value: FTable<'l>,
+    pub configuration: FConfiguration,
 }
 
-#[ffi_type]
-pub struct Outputs {
-    pub response: Response,
-    pub data: Data,
+#[ffi_type(namespace = "ffi")]
+pub struct FOutputs {
+    pub response: FResponse,
+    pub data: FData,
 }
 
-#[ffi_type]
-pub struct Context<'l> {
+#[ffi_type(namespace = "ffi")]
+pub struct FContext<'l> {
     pub things: ffi::Slice<'l, ffi::String>,
     // headers: HashMap<String, String>, // TODO: unsupported
 }
 
-#[ffi_type]
-pub struct Table<'l> {
-    pub metadata: TableMetadata,
+#[ffi_type(namespace = "ffi")]
+pub struct FTable<'l> {
+    pub metadata: FTableMetadata,
     pub byte_array: ffi::Slice<'l, u8>,
 }
 
-#[ffi_type]
-pub struct Configuration {
+#[ffi_type(namespace = "ffi")]
+pub struct FConfiguration {
     pub is_ok_response: bool,
     pub host: ffi::String,
     pub response_size: u64, // controls N in benchmarks
 }
 
-#[ffi_type]
-pub struct Response {
-    pub results: ffi::Vec<Result>,
+#[ffi_type(namespace = "ffi")]
+pub struct FResponse {
+    pub results: ffi::Vec<FResult>,
 }
 
-#[ffi_type]
-pub struct Data {
-    pub items: Items,
-    pub errors: Error,
+#[ffi_type(namespace = "ffi")]
+pub struct FData {
+    pub items: FItems,
+    pub errors: FError,
 }
 
-#[ffi_type]
-pub struct TableMetadata {
+#[ffi_type(namespace = "ffi")]
+pub struct FTableMetadata {
     pub row_count: i32,
     pub column_count: i32,
     pub guid: ffi::String,
     pub prefix: ffi::String,
 }
 
-#[ffi_type]
+#[ffi_type(namespace = "ffi")]
 #[derive(Clone)]
-pub struct Result {
+pub struct FResult {
     pub item_value: i32,
     pub item_id: ffi::String,
 }
 
-#[ffi_type]
-pub struct Items {
-    pub items: ffi::Vec<Item>,
+#[ffi_type(namespace = "ffi")]
+pub struct FItems {
+    pub items: ffi::Vec<FItem>,
 }
 
-#[ffi_type]
+#[ffi_type(namespace = "ffi")]
 #[derive(Clone)]
-pub enum ItemKey {
+pub enum FItemKey {
     TOTAL = 0,
     FIRST = 1,
     SECOND = 2,
     THIRD = 3,
 }
 
-#[ffi_type]
+#[ffi_type(namespace = "ffi")]
 #[derive(Clone)]
-pub struct Item {
-    pub key: ItemKey,
+pub struct FItem {
+    pub key: FItemKey,
     pub value: u64,
 }
 
-#[ffi_type]
-pub struct Error {
+#[ffi_type(namespace = "ffi")]
+pub struct FError {
     pub error_messages: ffi::Vec<ffi::String>,
 }
--- benchmarks/csharp_protobuf/interop/src/wire.rs
+++ benchmarks/csharp_protobuf/interop/src/wire.rs
@@ -1,4 +1,3 @@
-use super::wire;
 use interoptopus::{
     ffi_function, ffi_type,
     lang::{Wire, Wireable},
@@ -6,98 +5,98 @@ use interoptopus::{
 use std::collections::HashMap;
 
 /// Main benchmark entry point for wire based interop.
-#[ffi_function]
-fn WireRustClient(mut wire_input: Wire<wire::Input>) -> Wire<wire::Outputs> {
+#[ffi_function(namespace = "wire", debug)]
+fn WireRustClient(mut wire_input: Wire<WInput>) -> Wire<WOutputs> {
     let input = wire_input.unwire().unwrap();
     let output = rust_client_impl(input);
     output.wire()
 }
 
-fn rust_client_impl(_input: Input) -> Outputs {
+fn rust_client_impl(_input: WInput) -> WOutputs {
     // println!("rust_client_impl: {}, {}", _input.configuration.host, _input.configuration.response_size);
 
-    let results = vec![Result { item_value: 42, item_id: "item1".to_string() }];
-    let items = vec![Item { key: ItemKey::TOTAL, value: 100 }];
-    Outputs { response: Response { results: results }, data: Data { items: Items { items }, errors: Error { error_messages: vec![] } } }
+    let results = vec![WResult { item_value: 42, item_id: "item1".to_string() }];
+    let items = vec![WItem { key: WItemKey::TOTAL, value: 100 }];
+    WOutputs { response: WResponse { results: results }, data: WData { items: WItems { items }, errors: None } }
 }
 
-#[ffi_type(wired)]
-pub struct Input {
-    pub context: Context,
-    pub value: Table,
-    pub configuration: Configuration,
+#[ffi_type(wired, namespace = "wire", debug)]
+pub struct WInput {
+    pub context: WContext,
+    pub value: WTable,
+    pub configuration: WConfiguration,
 }
 
-#[ffi_type(wired)]
-pub struct Context {
+#[ffi_type(wired, namespace = "wire")]
+pub struct WContext {
     pub things: Vec<String>,
     pub headers: HashMap<String, String>,
 }
 
-#[ffi_type(wired)]
-pub struct TableMetadata {
+#[ffi_type(wired, namespace = "wire")]
+pub struct WTableMetadata {
     pub row_count: i32,
     pub column_count: i32,
     pub guid: String,
     pub prefix: String,
 }
 
-#[ffi_type(wired)]
-pub struct Table {
-    pub metadata: TableMetadata,
+#[ffi_type(wired, namespace = "wire")]
+pub struct WTable {
+    pub metadata: WTableMetadata,
     pub byte_array: Vec<u8>,
 }
 
-#[ffi_type(wired)]
-pub struct Configuration {
+#[ffi_type(wired, namespace = "wire")]
+pub struct WConfiguration {
     pub is_ok_response: bool,
     pub host: String,
     pub response_size: u64, // controls N in benchmarks
 }
 
-#[ffi_type(wired)]
-pub struct Outputs {
-    pub response: Response,
-    pub data: Data,
+#[ffi_type(wired, namespace = "wire", debug)]
+pub struct WOutputs {
+    pub response: WResponse,
+    pub data: WData,
 }
 
-#[ffi_type(wired)]
-pub struct Result {
+#[ffi_type(wired, namespace = "wire")]
+pub struct WResult {
     pub item_id: String,
     pub item_value: i32,
 }
 
-#[ffi_type(wired)]
-pub struct Response {
-    pub results: Vec<Result>,
+#[ffi_type(wired, namespace = "wire")]
+pub struct WResponse {
+    pub results: Vec<WResult>,
 }
 
-#[ffi_type(wired)]
-pub struct Data {
-    pub items: Items,
-    pub errors: Error,
+#[ffi_type(wired, namespace = "wire")]
+pub struct WData {
+    pub items: WItems,
+    pub errors: Option<WError>,
 }
 
-#[ffi_type(wired)]
-pub struct Items {
-    pub items: Vec<Item>,
+#[ffi_type(wired, namespace = "wire")]
+pub struct WItems {
+    pub items: Vec<WItem>,
 }
 
-#[ffi_type(wired)]
-pub enum ItemKey {
+#[ffi_type(wired, namespace = "wire")]
+pub enum WItemKey {
     TOTAL = 0,
     FIRST = 1,
     SECOND = 2,
     THIRD = 3,
 }
 
-#[ffi_type(wired)]
-pub struct Item {
-    pub key: ItemKey,
+#[ffi_type(wired, namespace = "wire")]
+pub struct WItem {
+    pub key: WItemKey,
     pub value: u64,
 }
 
-#[ffi_type(wired)]
-pub struct Error {
+#[ffi_type(wired, namespace = "wire")]
+pub struct WError {
     pub error_messages: Vec<String>,
 }
