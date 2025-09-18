73cbeaf08d43df0146eb80e3e9b11905c2135df2
Ralf Biedert <rb@xr.io>
2025-09-18 15:03:03 +0200

    Ty Claude.

--- crates/core/src/lang2/types/fnptr.rs
+++ crates/core/src/lang2/types/fnptr.rs
@@ -4,100 +4,149 @@ use crate::lang2::meta::{common_or_module_emission, Docs, Visibility};
 use crate::lang2::types::{Type, TypeId, TypeInfo, TypeKind};
 use crate::lang2::Register;
 
-impl<R: TypeInfo> TypeInfo for extern "C" fn() -> R {
-    fn id() -> TypeId {
-        TypeId::new(0xEE8602B016C043561CA68291A8142F3B).derive_id(R::id())
-    }
+macro_rules! impl_fnptr {
+    // No arguments: extern "C" fn() -> R
+    ($r:ident) => {
+        impl<$r: TypeInfo> TypeInfo for extern "C" fn() -> $r {
+            fn id() -> TypeId {
+                TypeId::new(0xEE8602B016C043561CA68291A8142F3B).derive_id($r::id())
+            }
+        }
+
+        impl<$r: TypeInfo + Register> Register for extern "C" fn() -> $r {
+            fn register(inventory: &mut Inventory) {
+                $r::register(inventory);
+
+                let r = &inventory.types[&$r::id()];
+                let ty_params = [inventory.types[&$r::id()].emission.clone()];
+                let emission = common_or_module_emission(&ty_params);
+                let signature = Signature {
+                    arguments: vec![],
+                    rval: $r::id()
+                };
+
+                let type_ = Type {
+                    emission,
+                    docs: Docs::empty(),
+                    visibility: Visibility::Public,
+                    name: format!(r#"extern "C" fn() -> {}"#, r.name),
+                    kind: TypeKind::FnPointer(signature),
+                };
+
+                inventory.register_type(Self::id(), type_);
+            }
+        }
+
+        impl<$r: TypeInfo> TypeInfo for Option<extern "C" fn() -> $r> {
+            fn id() -> TypeId {
+                <extern "C" fn() -> $r as TypeInfo>::id()
+            }
+        }
+
+        impl<$r: TypeInfo + Register> Register for Option<extern "C" fn() -> $r> {
+            fn register(inventory: &mut Inventory) {
+                <extern "C" fn() -> $r as Register>::register(inventory);
+            }
+        }
+    };
+
+    // With arguments: extern "C" fn(T1, T2, ...) -> R
+    ($r:ident, $($t:ident),+) => {
+        impl<$r, $($t),+> TypeInfo for extern "C" fn($($t),+) -> $r
+        where
+            $($t: TypeInfo,)+
+            $r: TypeInfo,
+        {
+            fn id() -> TypeId {
+                TypeId::new(0xEE8602B016C043561CA68291A8142F3B)
+                    .derive_id($r::id())
+                    $(.derive_id($t::id()))+
+            }
+        }
+
+        #[allow(non_snake_case, unused_assignments)]
+        impl<$r, $($t),+> Register for extern "C" fn($($t),+) -> $r
+        where
+            $($t: Register + TypeInfo,)+
+            $r: Register + TypeInfo,
+        {
+            fn register(inventory: &mut Inventory) {
+                $r::register(inventory);
+                $($t::register(inventory);)+
+
+                let r = &inventory.types[&$r::id()];
+                $(let $t = &inventory.types[&$t::id()];)+
+
+                let ty_params = [
+                    inventory.types[&$r::id()].emission.clone(),
+                    $(inventory.types[&$t::id()].emission.clone()),+
+                ];
+                let emission = common_or_module_emission(&ty_params);
+
+                let arguments = {
+                    let mut args = Vec::new();
+                    let mut counter = 1;
+                    $(
+                        args.push(Argument {
+                            string: format!("x{}", counter),
+                            ty: $t::id()
+                        });
+                        counter += 1;
+                    )+
+                    args
+                };
+
+                let signature = Signature {
+                    arguments,
+                    rval: $r::id()
+                };
+
+                let type_ = Type {
+                    emission,
+                    docs: Docs::empty(),
+                    visibility: Visibility::Public,
+                    name: format!(r#"extern "C" fn({}) -> {}"#,
+                        [$($t.name.clone()),+].join(", "),
+                        r.name
+                    ),
+                    kind: TypeKind::FnPointer(signature),
+                };
+
+                inventory.register_type(Self::id(), type_);
+            }
+        }
+
+        impl<$r, $($t),+> TypeInfo for Option<extern "C" fn($($t),+) -> $r>
+        where
+            $($t: TypeInfo,)+
+            $r: TypeInfo,
+        {
+            fn id() -> TypeId {
+                <extern "C" fn($($t),+) -> $r as TypeInfo>::id()
+            }
+        }
+
+        impl<$r, $($t),+> Register for Option<extern "C" fn($($t),+) -> $r>
+        where
+            $($t: TypeInfo + Register,)+
+            $r: TypeInfo + Register,
+        {
+            fn register(inventory: &mut Inventory) {
+                <extern "C" fn($($t),+) -> $r as Register>::register(inventory);
+            }
+        }
+    };
 }
 
-impl<R: TypeInfo + Register> Register for extern "C" fn() -> R {
-    fn register(inventory: &mut Inventory) {
-        R::register(inventory);
-
-        let r = &inventory.types[&R::id()];
-
-        let ty_params = [inventory.types[&R::id()].emission.clone()];
-        let emission = common_or_module_emission(&ty_params);
-        let signature = Signature { arguments: vec![], rval: R::id() };
-
-        let type_ = Type {
-            emission,
-            docs: Docs::empty(),
-            visibility: Visibility::Public,
-            name: format!(r#"extern "C" fn() -> {}"#, r.name),
-            kind: TypeKind::FnPointer(signature),
-        };
-
-        inventory.register_type(Self::id(), type_);
-    }
-}
-
-impl<R: TypeInfo> TypeInfo for Option<extern "C" fn() -> R> {
-    fn id() -> TypeId {
-        <extern "C" fn() -> R as TypeInfo>::id()
-    }
-}
-
-impl<R: TypeInfo + Register> Register for Option<extern "C" fn() -> R> {
-    fn register(inventory: &mut Inventory) {
-        <extern "C" fn() -> R as Register>::register(inventory);
-    }
-}
-
-impl<R, T1> TypeInfo for extern "C" fn(T1) -> R
-where
-    T1: TypeInfo,
-    R: TypeInfo,
-{
-    fn id() -> TypeId {
-        TypeId::new(0xEE8602B016C043561CA68291A8142F3B).derive_id(R::id()).derive_id(T1::id())
-    }
-}
-
-impl<R, T1> Register for extern "C" fn(T1) -> R
-where
-    T1: Register + TypeInfo,
-    R: Register + TypeInfo,
-{
-    fn register(inventory: &mut Inventory) {
-        R::register(inventory);
-        T1::register(inventory);
-
-        let r = &inventory.types[&R::id()];
-        let t1 = &inventory.types[&T1::id()];
-
-        let ty_params = [inventory.types[&R::id()].emission.clone(), inventory.types[&T1::id()].emission.clone()];
-        let emission = common_or_module_emission(&ty_params);
-        let signature = Signature { arguments: vec![Argument { string: "x1".to_string(), ty: T1::id() }], rval: R::id() };
-
-        let type_ = Type {
-            emission,
-            docs: Docs::empty(),
-            visibility: Visibility::Public,
-            name: format!(r#"extern "C" fn({}) -> {}"#, t1.name, r.name),
-            kind: TypeKind::FnPointer(signature),
-        };
-
-        inventory.register_type(Self::id(), type_);
-    }
-}
-
-impl<R, T1> TypeInfo for Option<extern "C" fn(T1) -> R>
-where
-    T1: TypeInfo,
-    R: TypeInfo,
-{
-    fn id() -> TypeId {
-        <extern "C" fn(T1) -> R as TypeInfo>::id()
-    }
-}
-
-impl<R, T1> Register for Option<extern "C" fn(T1) -> R>
-where
-    T1: TypeInfo + Register,
-    R: TypeInfo + Register,
-{
-    fn register(inventory: &mut Inventory) {
-        <extern "C" fn(T1) -> R as Register>::register(inventory);
-    }
-}
+// Generate implementations for function pointers with different arities
+impl_fnptr!(R);
+impl_fnptr!(R, T1);
+impl_fnptr!(R, T1, T2);
+impl_fnptr!(R, T1, T2, T3);
+impl_fnptr!(R, T1, T2, T3, T4);
+impl_fnptr!(R, T1, T2, T3, T4, T5);
+impl_fnptr!(R, T1, T2, T3, T4, T5, T6);
+impl_fnptr!(R, T1, T2, T3, T4, T5, T6, T7);
+impl_fnptr!(R, T1, T2, T3, T4, T5, T6, T7, T8);
+impl_fnptr!(R, T1, T2, T3, T4, T5, T6, T7, T8, T9);
+impl_fnptr!(R, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
