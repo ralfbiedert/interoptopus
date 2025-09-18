3b0e0d8eb93368b43e2b12695a5a5da2caffdc64
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-09-18 12:54:17 +0300

    Work on nested types serde

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,16 +1,35 @@
-use interoptopus::{ffi_function, ffi_type};
+use interoptopus::{callback, ffi, ffi_function, ffi_type, wire::Wire};
+use std::collections::HashMap;
 
-/// A simple type in our FFI layer.
-#[ffi_type]
-pub struct Vec2 {
-    pub x: f32,
-    pub y: f32,
+callback!(CsharpCallback(result: interoptopus::ffi::Slice<u8>));
+
+#[ffi_type(wired)]
+pub struct Pair {
+    pub key: u32,
+    pub value: Option<String>,
 }
 
-/// Function using the type.
+impl std::fmt::Display for Pair {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.key, self.value.as_deref().unwrap_or("??"))
+    }
+}
+
+#[ffi_type(wired)]
+pub struct Fields {
+    pub name: String,
+    pub attrs: HashMap<String, Pair>, // Demo: replace String with Pair
+}
+
 #[ffi_function]
-pub fn my_function(input: Vec2) -> Vec2 {
-    input
+pub fn concat(mut input: Wire<Fields>, callback: CsharpCallback) -> ffi::String {
+    let fields = input.unwire().expect("All shall be good whoohoo");
+
+    let output = format!("{}: {}", fields.name, fields.attrs.iter().fold(String::new(), |acc, (k, v)| format!("{acc} {k}={v}")));
+
+    callback.call(interoptopus::ffi::Slice::from_slice(output.as_bytes()));
+
+    ffi::String::from_string(output)
 }
 
 // We just trick a unit test into producing our bindings, here for C#
@@ -24,7 +43,7 @@ fn generate_bindings() {
     // In a real project this should be a freestanding `my_inventory()` function inside
     // your FFI or build crate.
     let inventory = Inventory::builder()
-        .register(function!(my_function))
+        .register(function!(concat))
         .validate()
         .build();
 
