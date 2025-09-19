48b1005601e5f5eb240a5df8227cdc40441437bd
Ralf Biedert <rb@xr.io>
2025-09-19 21:09:47 +0200

    Emit right repr.

--- crates/proc_macros/src/lib.rs
+++ crates/proc_macros/src/lib.rs
@@ -8,7 +8,7 @@ mod types;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_attribute] // Can now be used as `#[my_attribute]`
+#[proc_macro_attribute]
 pub fn ffi_type(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attr = proc_macro2::TokenStream::from(attr);
     let input = proc_macro2::TokenStream::from(item);
