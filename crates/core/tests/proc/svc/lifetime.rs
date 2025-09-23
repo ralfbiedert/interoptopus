e11db02309bfdbd8ed991a46f1d19c2a9de48eb5
Ralf Biedert <rb@xr.io>
2025-09-23 21:29:18 +0200

    Towards getting lifetimes working again.

---
+++ crates/core/tests/proc/svc/lifetime.rs
@@ -0,0 +1,30 @@
+use interoptopus::{ffi, ffi_service, ffi_type};
+
+#[ffi_type]
+enum Error {
+    Something,
+}
+
+#[ffi_type(service)]
+struct Service {
+    pub data: Vec<u32>,
+}
+
+#[ffi_service]
+impl Service {
+    pub fn new() -> ffi::Result<Self, Error> {
+        ffi::Ok(Self { data: Vec::new() })
+    }
+
+    #[allow(clippy::needless_lifetimes)]
+    pub fn l1<'a, 'b>(&mut self, x: &u8, _y: &mut u8, _slice: ffi::Slice<'a, u8>, _slice2: ffi::Slice<'b, u8>) -> u8 {
+        *x
+    }
+
+    pub fn return_slice(&mut self) -> ffi::Slice<'_, u32> {
+        self.data.as_slice().into()
+    }
+}
+
+#[allow(dead_code)]
+fn main() {}
