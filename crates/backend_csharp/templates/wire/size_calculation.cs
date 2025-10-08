4b2d03acbf57fc2c844fb55501478ce5d27e038e
Berkus Karchebnyi <bkarchebnyi@microsoft.com>
2025-10-08 19:24:14 +0300

    fix: 🐛 Fix wrong codegen for wire of Option<String>

--- crates/backend_csharp/templates/wire/size_calculation.cs
+++ crates/backend_csharp/templates/wire/size_calculation.cs
@@ -11,7 +11,11 @@ WireInterop.CalculateVariableVecSize(this.{{field.name}}, Serde{{field.inner_typ
 {% set kv = field.inner_type | split(pat=", ") %}
 WireInterop.CalculateVariableMapSize(this.{{field.name}}, Serde{{kv[0]}}Extensions.CalculateSize, Serde{{kv[1]}}Extensions.CalculateSize) /* {{field.kind}} */
 {%- elif field.kind == "optional" %}
-1 + (this.{{field.name}} != null ? Marshal.SizeOf<{{field.inner_type}}>() : 0) /* {{field.kind}} */
+{%- if field.inner_type == "String" %}
+1 + (this.{{field.name}} != null ? Marshal.SizeOf<ulong>() + System.Text.Encoding.UTF8.GetByteCount(this.{{field.name}} ?? "") : 0) /* {{field.kind}} */
+{% else %}
+1 + (this.{{field.name}} != null ? Marshal.SizeOf<{{field.inner_type}}>() : 0) /* {{field.kind}} of {{field.inner_type}} */
+{% endif %}
 {%- elif field.kind == "enum" %}
 Marshal.SizeOf<ulong>() /* {{field.kind}} */
 {%- elif field.kind == "primitive" %}
