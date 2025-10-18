c9fbf3efa9709181414ea0c057a2763c5674ab48
Ralf Biedert <rb@xr.io>
2025-10-18 22:37:39 +0200

    Nuke old proc macros.

--- crates/proc_macros_old/src/util/mod.rs
+++
@@ -1,9 +0,0 @@
-mod debug;
-mod docs;
-mod strings;
-mod types;
-
-pub use debug::prettyprint_tokenstream;
-pub use docs::extract_doc_lines;
-pub use strings::pascal_to_snake_case;
-pub use types::{ReplaceSelf, get_type_name, purge_lifetimes_from_type};
