d34b9d380fd5a62404942c3ff4b3801ffac16dfa
Ralf Biedert <rb@xr.io>
2025-11-16 19:29:07 +0100

    Add other types.

--- crates/backend_csharp_ng/src/lang/types/csharp.rs
+++ crates/backend_csharp_ng/src/lang/types/csharp.rs
@@ -1,4 +1,6 @@
 use crate::model::TypeId;
 
-// A C# `string`
-const STRING: TypeId = TypeId::new(0xBEA1AB15FD5682B7649431E17CB70B61);
+// A C# `string`, and the extra generated types `CStrPtr` and `Utf8String`
+const String: TypeId = TypeId::new(0xBEA1AB15FD5682B7649431E17CB70B61);
+const CStrPtr: TypeId = TypeId::new(0x23A15DA804954D6FA2092D2FA2177E7C);
+const Utf8String: TypeId = TypeId::new(0xA6E549EB2961D56F3AA8A3BDAF748FDC);
