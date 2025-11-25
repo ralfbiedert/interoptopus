f956ee8ae52b3fb77fa332c790de0b0ff32e13bd
Ralf Biedert <rb@xr.io>
2025-11-25 16:27:05 +0100

    Make it compile again.

--- crates/backend_csharp/src/interop/builtins/async_helper.rs
+++
@@ -1,9 +0,0 @@
-use crate::Interop;
-use interoptopus_backend_utils::{Error, IndentWriter, render};
-
-pub fn write_async_helper(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
-    if i.write_types.write_interoptopus_globals() {
-        render!(w, "builtins/async_helper.cs")?;
-    }
-    Ok(())
-}
--- crates/backend_csharp/src/interop/builtins/exception.rs
+++
@@ -1,9 +0,0 @@
-use crate::Interop;
-use interoptopus_backend_utils::{Error, IndentWriter, render};
-
-pub fn write_interop_exception(i: &Interop, w: &mut IndentWriter) -> Result<(), Error> {
-    if i.write_types.write_interoptopus_globals() {
-        render!(w, "builtins/exception.cs")?;
-    }
-    Ok(())
-}
