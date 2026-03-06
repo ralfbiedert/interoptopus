c992b0a565878ec6607c2448973917ecce6bf4bd
Ralf Biedert <rb@xr.io>
2026-03-06 17:16:36 +0100

    Start reactivating `hello_world` example; but need real routing for outputs now.

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,14 +1,16 @@
-use interoptopus::{ffi_function, ffi_type};
+use interoptopus::ffi;
+use interoptopus::inventory::RustInventory;
+use interoptopus_csharp::RustLibrary;
 
 /// A simple type in our FFI layer.
-#[ffi_type]
+#[ffi]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
 
 /// Function using the type.
-#[ffi_function]
+#[ffi]
 pub fn my_function(input: Vec2) -> Vec2 {
     input
 }
@@ -16,20 +18,23 @@ pub fn my_function(input: Vec2) -> Vec2 {
 // We just trick a unit test into producing our bindings, here for C#
 #[test]
 #[rustfmt::skip]
-fn generate_bindings() {
+fn generate_bindings() -> Result<(), Box<dyn std::error::Error>> {
     use interoptopus::function;
     use interoptopus::inventory::Inventory;
-    use interoptopus_backend_csharp::Interop;
 
     // In a real project this should be a freestanding `my_inventory()` function inside
     // your FFI or build crate.
-    let inventory = Inventory::builder()
+    let inventory = RustInventory::new()
         .register(function!(my_function))
-        .validate()
-        .build();
+        .validate();
 
-    Interop::builder()
-        .inventory(inventory)
-        .build().unwrap()
-        .write_file("bindings/Interop.cs").unwrap()
+    let multibuf = RustLibrary::builder(inventory)
+        .dll_name("hello_world")
+        .build()
+        .process()?;
+
+    // _ = multibuf.write_buffer("bindings/Interop.cs")?;
+    _ = multibuf.write_buffer("Foo.cs")?;
+
+    Ok(())
 }
