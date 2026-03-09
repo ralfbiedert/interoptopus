0bf28fb8c64458aa328b1031c286cebc599c50ab
Ralf Biedert <rb@xr.io>
2026-03-09 10:50:57 +0100

    Fix caller.

--- examples/hello_world/bindings/Test.Invoke.cs
+++ examples/hello_world/bindings/Test.Invoke.cs
@@ -6,8 +6,8 @@ public class TestInvoke
     [Fact]
     public void Invoke()
     {
-        var vec1 = new Interop.Vec2 { x = 1.0f, y = 2.0f };
-        var vec2 = A.Interop.my_function(vec1);
+        var vec1 = new Vec2() { x = 1.0f, y = 2.0f };
+        var vec2 = Interop.my_function(vec1);
     }
 
 }
