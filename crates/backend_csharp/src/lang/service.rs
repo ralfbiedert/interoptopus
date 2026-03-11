80b2126454384a3fc5c7a04a2e2db5001a1eedc4
Ralf Biedert <rb@xr.io>
2026-03-11 14:13:29 +0100

    Cleanup.

--- crates/backend_csharp/src/lang/service.rs
+++ crates/backend_csharp/src/lang/service.rs
@@ -5,7 +5,6 @@ new_id!(ServiceIdCs);
 
 pub struct Service {
     pub ty: TypeId,
-    // TODO: Should overloads for service methods be stored here?
     pub ctors: Vec<FunctionId>,   // These are interop functions, we might have more overloads
     pub methods: Vec<FunctionId>, // These are interop functions, we might have more overloads
     pub destructor: FunctionId,
