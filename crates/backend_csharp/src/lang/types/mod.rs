88cf4b8e2265e885a3ab8ef1ab5a555f19878d0b
Ralf Biedert <rb@xr.io>
2026-03-16 13:52:49 +0100

    Start introducing decorators.

--- crates/backend_csharp/src/lang/types/mod.rs
+++ crates/backend_csharp/src/lang/types/mod.rs
@@ -17,8 +17,37 @@ pub enum ManagedConversion {
 }
 
 #[derive(Clone, Debug)]
+pub enum MarshalAs {
+    LPStr,
+}
+
+/// Decorators for function / method parameters.
+#[derive(Clone, Debug)]
+pub enum ParamDecorator {
+    Ref,
+    Out,
+    MarshalAs(MarshalAs),
+}
+
+impl ParamDecorator {
+    pub fn to_string(&self) -> String {
+        match self {
+            ParamDecorator::Ref => "ref".to_string(),
+            ParamDecorator::Out => "out".to_string(),
+            ParamDecorator::MarshalAs(m) => format!("[MarshalAs({})]", m),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Decorators {
+    pub param: Option<ParamDecorator>,
+}
+
+#[derive(Clone, Debug)]
 pub struct Type {
     pub emission: Emission,
     pub name: String,
     pub kind: TypeKind,
+    pub decorators: Decorators,
 }
