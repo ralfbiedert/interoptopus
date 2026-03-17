58c67987861c0673267beebdb35a98529a374526
Ralf Biedert <rb@xr.io>
2026-03-17 11:16:44 +0100

    Start clean up example projects.

--- crates/proc_macros/src/lib.rs
+++ crates/proc_macros/src/lib.rs
@@ -5,12 +5,13 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
-/// See [`interoptopus::proc::ffi`](https://docs.rs/interoptopus/latest/interoptopus/proc/macro.ffi.html) for full documentation.
+/// See [`interoptopus::ffi`](https://docs.rs/interoptopus/latest/interoptopus/macro.ffi.html) for full documentation.
 #[proc_macro_attribute]
 pub fn ffi(attr: TokenStream, item: TokenStream) -> TokenStream {
     interoptopus_proc_impl::ffi(attr.into(), item.into()).into()
 }
 
+/// See [`interoptopus::AsyncRuntime`](https://docs.rs/interoptopus/latest/interoptopus/macro.AsyncRuntime.html) for full documentation.
 #[proc_macro_derive(AsyncRuntime, attributes(runtime))]
 pub fn derive_async_runtime(item: TokenStream) -> TokenStream {
     interoptopus_proc_impl::derive_async_runtime(item.into()).into()
--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,7 +1,4 @@
-use interoptopus::pattern::asynk::Async;
-use interoptopus::pattern::result::result_to_ffi;
-use interoptopus::rt::Tokio;
-use interoptopus::{AsyncRuntime, callback, ffi};
+use interoptopus::ffi;
 
 /// A simple type in our FFI layer.
 #[ffi]
@@ -10,74 +7,16 @@ pub struct Vec2 {
     pub y: f32,
 }
 
-/// A simple type in our FFI layer.
-#[ffi]
-pub enum Error {
-    A,
-    B(u32),
-}
-
-callback!(SumDelegate2(x: i32, y: i32) -> i32);
-callback!(SumDelegateReturn(x: i32, y: i32) -> ffi::Result<(), Error>);
-callback!(SumDelegateReturn2(x: i32, y: i32));
-
 /// Function using the type.
 #[ffi]
 pub fn my_function(input: Vec2) -> Vec2 {
     input
 }
 
-#[ffi]
-pub fn refref(input: &u32) -> &u32 {
-    input
-}
-
-#[ffi]
-pub fn delgt(x: SumDelegateReturn, _input: &u32, _input2: &mut u32) -> SumDelegateReturn {
-    x
-}
-
-#[ffi(service)]
-pub struct ServiceBasic {}
-
-#[ffi]
-impl ServiceBasic {
-    pub fn new() -> ffi::Result<Self, Error> {
-        ffi::Ok(Self {})
-    }
-
-    pub fn sum(&self, x: i32, y: i32) -> i32 {
-        x + y
-    }
-
-    pub fn delgt(&mut self, x: SumDelegateReturn, _input: &u32, _input2: &u32) -> SumDelegateReturn {
-        x
-    }
-}
-
-#[ffi(service)]
-#[derive(AsyncRuntime)]
-pub struct ServiceBasic2 {
-    runtime: Tokio,
-}
-
-#[ffi]
-impl ServiceBasic2 {
-    pub fn new() -> ffi::Result<Self, Error> {
-        result_to_ffi(|| {
-            let runtime = Tokio::new();
-            Ok(Self { runtime })
-        })
-    }
-    pub async fn sum(_this: Async<Self>, _x: i32, _y: i32, _z: SumDelegateReturn) -> ffi::Result<(), Error> {
-        ffi::Result::Ok(())
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use interoptopus::function;
     use interoptopus::inventory::RustInventory;
-    use interoptopus::{extra_type, function, service};
     use interoptopus_csharp::RustLibrary;
     use interoptopus_csharp::dispatch::Dispatch;
     use interoptopus_csharp::lang::meta::FileEmission;
@@ -88,16 +27,7 @@ mod tests {
     fn generate_bindings() -> Result<(), Box<dyn std::error::Error>> {
         // In a real project this should be a freestanding `my_inventory()` function inside
         // your FFI or build crate.
-        let inventory = RustInventory::new()
-            .register(function!(super::my_function))
-            .register(function!(super::refref))
-            .register(function!(super::delgt))
-            .register(extra_type!(super::Error))
-            .register(extra_type!(super::SumDelegate2))
-            .register(extra_type!(super::SumDelegateReturn))
-            .register(service!(super::ServiceBasic))
-            .register(service!(super::ServiceBasic2))
-            .validate();
+        let inventory = RustInventory::new().register(function!(super::my_function)).validate();
 
         RustLibrary::builder(inventory)
             .dll_name("hello_world")
