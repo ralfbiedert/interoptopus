ecf1c8e80c8443f6a5d9afc68661f3ab33b4038a
Ralf Biedert <rb@xr.io>
2026-03-21 12:21:12 +0100

    Actually test output.

--- crates/backend_csharp/tests/reference_plugins/wire.rs
+++ crates/backend_csharp/tests/reference_plugins/wire.rs
@@ -1,6 +1,5 @@
 use crate::{define_plugin, load_plugin};
 use interoptopus::wire::Wire;
-use reference_project::plugins::service::Service;
 use reference_project::plugins::wire::Wired;
 use std::collections::HashMap;
 use std::error::Error;
@@ -19,12 +18,12 @@ fn load_plugin() -> Result<(), Box<dyn Error>> {
 
     let hashmap = HashMap::from([("foo".to_string(), "bar".to_string())]);
     let result = plugin.wire_hashmap_string(Wire::from(hashmap)).unwire();
-
     assert_eq!(result.get("foo").map(String::as_str), Some("bar"));
     assert_eq!(result.get("hello").map(String::as_str), Some("world"));
 
     let result = plugin.wire_string(Wire::from("{}".to_string())).unwire();
-    dbg!(result);
+    assert!(result.contains("hello"));
+    assert!(result.contains("world"));
 
     Ok(())
 }
