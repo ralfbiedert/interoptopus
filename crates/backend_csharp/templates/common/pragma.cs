008516e3a811a67adaf73debccae7422cac1bdb3
Ralf Biedert <rb@xr.io>
2026-04-10 21:22:59 +0200

    Disable more warnings in generated code.

--- crates/backend_csharp/templates/common/pragma.cs
+++ crates/backend_csharp/templates/common/pragma.cs
@@ -10,3 +10,4 @@
 #pragma warning disable CS8602 // Dereference of a possibly null reference
 #pragma warning disable CS8603 // Possible null reference return
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type
